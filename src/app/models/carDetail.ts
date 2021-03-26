3a3ea9623bb78e4ba254c72da1cda206bce8f4e3
Büşra <bsrtmr92@hotmail.com>
2021-03-27 02:27:18 +0300

    Listeleme Yapıldı

---
+++ src/app/models/carDetail.ts
@@ -0,0 +1,8 @@
+export interface CarDetail{
+    carId:number,
+    brandName:string,
+    modelYear:number,
+    colorName:string,
+    dailyPrice:number,
+    description:string
+}
---
+++ src/app/services/rental.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ListResponseModel } from '../models/listResponseModel';
+import { RentalDetail } from '../models/rentalDetail';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RentalService {
+
+  apiUrl = 'https://localhost:44331/api/rentals/getrentaldetail';
+
+  constructor(private httpClient: HttpClient) { }
+
+  getRentals():Observable<ListResponseModel<RentalDetail>> {
+    return this.httpClient.get<ListResponseModel<RentalDetail>>(this.apiUrl);
+  }
+}
